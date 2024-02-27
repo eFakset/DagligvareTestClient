c3de4996e22454bdb17579c681e5790eb436fae3
Erik Rosness <erik@rosness.no>
2024-02-27 22:09:28 +0100

    Get rid of redundant files

--- dberror.js
+++
@@ -1,5 +0,0 @@
-function kommuner() {
-    return fetch("http://localhost:3001/kommuner").then((rsp)=>(rsp.json()));
-}
-
-module.exports = kommuner;
--- noserver.js
+++
@@ -1,5 +0,0 @@
-function kommuner(port) {
-    return fetch("http://localhost:" + port.toString() + "/kommuner").then((rsp)=>(rsp.json()));
-}
-
-module.exports = kommuner;
